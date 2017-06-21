fd8c9685d5d23a7f6bf229a77a3e3b33974cbb9d
walmav <madhu@skytale.io>
2017-06-21 12:48:07 -0700

    initial commit for DEMO on 06/22/2017

---
+++ skeleton/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/walmav/microservice/skeleton/ms"
+	"log"
+	"net/http"
+
+)
+
+func main() {
+	svc := ms.NodeImpl{}
+
+	GetIdHandler := httptransport.NewServer(
+		ms.MakeGetIdEndpoint(svc),
+		ms.DecodeGetIdEndpointRequest,
+		ms.EncodeResponse,
+	)
+	http.Handle("/getId", GetIdHandler)
+	log.Print(http.ListenAndServe(":8080", nil))
+}
